ba9bbd1aecf2281fdc46f48046ab2b7ff4650c3f
Dan Gillespie <dan@redspread.com>
2016-02-19 21:51:13 -0800

    working one command deploy

--- docker-entry.sh
+++ docker-entry.sh
@@ -4,20 +4,25 @@ echo -ne "Configure database connection..."
 if [ ! -f $config ]
 then
     cp /config.template.json $config
-    sed -Ei "s/DB_PORT/$MATTERMOST_DB_SERVICE_PORT/" $config
+    PGUSER=$(</etc/pgsecrets/username)
+    PGPASS=$(</etc/pgsecrets/password)
+
+    sed -i -e "s/POSTGRES_USER/$PGUSER/g" $config
+    sed -i -e "s/POSTGRES_PASSWORD/$PGPASS/g" $config
+
     echo OK
 else
     echo SKIP
 fi
 
 echo "Wait until database is ready..."
-until nc -z mattermost-db $MATTERMOST_DB_SERVICE_PORT
+until nc -z localhost 5432
 do
     sleep 1
 done
 
 # Wait to avoid "panic: Failed to open sql connection pq: the database system is starting up"
-sleep 1
+sleep 5
 
 echo "Starting platform"
 cd /mattermost/bin
