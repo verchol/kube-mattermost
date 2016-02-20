f562a10349e5cfd51cbde0e16dbeb52e93ef172d
Dan Gillespie <dan@redspread.com>
2016-02-19 20:19:40 -0800

    moved to spread format with single pod

--- db/make_db.sh
+++
@@ -1,7 +0,0 @@
-export PGUSER=postgres
-# Username and password gets changed here
-psql <<- EOSQL
-    CREATE DATABASE mattermost;
-    CREATE USER mmuser WITH PASSWORD 'mmuser_password';
-    GRANT ALL PRIVILEGES ON DATABASE mattermost to mmuser;
-EOSQL
